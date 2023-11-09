7a876fda7f531eaa97cc3fb8a5ca60fb976e4d49
Charles Montgomery <charles.montgomery@charter.net>
2023-11-08 20:31:00 -0600

    Reimplementing Commandline processing

--- multi_image_process.py
+++ multi_image_process.py
@@ -4,6 +4,89 @@ import pathlib
 import sys
 import subprocess
 import time
+import command_line_parser
+import traceback
+from command_line_parser import CommandLineValue
+bconvert=CommandLineValue.bool_as_string
+class MultiImageCommandLineOptionsHydrator(command_line_parser.BaseHelpfulCommandLineHydrator):
+    def __init__(self) -> None:
+        super().__init__()
+    def _createOptions(self):
+        return MultiImageCommandLineOptions()
+class MultiImageCommandLineOptions(command_line_parser.BaseHelpfulCommandLineOptions):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__input_directory:str=None
+        self.__output_dir:str=None
+        self.__colorhex:str=None
+        self.__invert:bool=None
+        self.__cell_invert:bool=None
+        self.__multiprocessing:bool=None
+        self.__reset_output_dir:bool=None
+    def __fill_autos(self):
+        (self.__invert,self.__cell_invert,self.__multiprocessing,self.__reset_output_dir) = (False,False,False,False)
+    def __sub_validate(self) -> bool:
+        self.__fill_autos()
+        return self.__input_directory is not None \
+            and self.__output_dir is not None
+    
+    def validate(self) -> bool:
+        return super().validate() \
+            and self.__sub_validate()    
+    def _populate_options(self) -> None:
+        super()._populate_options()
+        def set_input_directory_cmd(key:str,value:command_line_parser.CommandLineValue):
+            if key == 'input_directory' and self.__input_directory is None:
+                self.__input_directory = value.string_value
+        def set_output_dir_cmd(key:str,value:command_line_parser.CommandLineValue):
+            if key == 'output_dir' and self.__output_dir is None:
+                self.__output_dir = value.string_value
+        def set_colorhex_cmd(key:str,value:command_line_parser.CommandLineValue):
+            if key == 'colorhex' and self.__colorhex is None:
+                self.__colorhex = value.string_value
+        def set_invert_cmd(key:str,value:command_line_parser.CommandLineValue):
+            if key == 'invert' and self.__invert is None:
+                self.__invert = value.bool_value
+        def set_cell_invert_cmd(key:str,value:command_line_parser.CommandLineValue):
+            if key == 'cell_invert' and self.__cell_invert is None:
+                self.__cell_invert = value.bool_value
+        def set_multiprocessing_cmd(key:str,value:command_line_parser.CommandLineValue):
+            if key == 'multiprocessing' and self.__multiprocessing is None:
+                self.__multiprocessing = value.bool_value
+        def set_reset_output_dir_cmd(key:str,value:command_line_parser.CommandLineValue):
+            if key == 'reset_output_dir' and self.__reset_output_dir is None:
+                self.__reset_output_dir = value.bool_value
+        self._populate_option('input_directory',command_line_parser.BaseHelpfulCommandLineOption('',set_input_directory_cmd))
+        self._populate_option('output_dir',command_line_parser.BaseHelpfulCommandLineOption('',set_output_dir_cmd))
+        self._populate_option('colorhex',command_line_parser.BaseHelpfulCommandLineOption('',set_colorhex_cmd))
+        self._populate_option('invert',command_line_parser.BaseHelpfulCommandLineOption('',set_invert_cmd))
+        self._populate_option('cell_invert',command_line_parser.BaseHelpfulCommandLineOption('',set_cell_invert_cmd))
+        self._populate_option('multiprocessing',command_line_parser.BaseHelpfulCommandLineOption('',set_multiprocessing_cmd))
+        self._populate_option('reset_output_dir',command_line_parser.BaseHelpfulCommandLineOption('',set_reset_output_dir_cmd))
+    @property 
+    def reset_output_dir(self):
+        return self.__reset_output_dir
+    @property
+    def input_directory(self):
+        return self.__input_directory
+    @property
+    def output_dir(self):
+        return self.__output_dir
+    @property
+    def colorhex(self):
+        return self.__colorhex
+    @property
+    def invert(self):
+        return self.__invert
+    @property
+    def cell_invert(self):
+        return self.__cell_invert
+    @property
+    def multiprocessing(self):
+        return self.__multiprocessing
+        
+
+
 def subprocess_it(img_name:str,img_out_name:str,colorhex:str,invert,cell_invert,ppool:list[list[subprocess.Popen]]):
     found=False    
     while not found:
@@ -17,8 +100,9 @@ def subprocess_it(img_name:str,img_out_name:str,colorhex:str,invert,cell_invert,
             else:
                 idx += 1
         time.sleep(0.1)
-def subprocess_do(img_name:str,img_out_name:str,colorhex:str,invert,cell_invert,pref:list[subprocess.Popen]):                
-    pref[0]=subprocess.Popen(args=[sys.executable,'image_process.py',img_name,img_out_name,colorhex if colorhex is not None else '','y' if invert else 'n','y' if cell_invert else 'n'])
+def subprocess_do(img_name:str,img_out_name:str,colorhex:str,invert,cell_invert,pref:list[subprocess.Popen]):            
+    args=[g for f in [[sys.executable,'image_process.py','--img_name',img_name,'--img_out_name',img_out_name],[] if colorhex is None else ['--colorhex',colorhex],[] if not invert else ['--invert'],[] if not cell_invert else ['--cell_invert']] for g in f]    
+    pref[0]=subprocess.Popen(args=args)
     #print('Process Executing...')
 def drain(ppool:list[list[subprocess.Popen]]):
     found=True
@@ -29,12 +113,12 @@ def drain(ppool:list[list[subprocess.Popen]]):
                 found=True
                 break
         time.sleep(0.1)
-def main(input_directory: str,output_dir: str,reset_output_dir: bool=False,colorhex:str=None,invert=False,cell_invert=False,multiprocessing=False):
-    opath=pathlib.Path(output_dir)
-    ipath=pathlib.Path(input_directory)
+def main(args:MultiImageCommandLineOptions):
+    opath=pathlib.Path(args.output_dir)
+    ipath=pathlib.Path(args.input_directory)
     if not opath.exists():
-        os.mkdir(output_dir)
-    if reset_output_dir:
+        os.mkdir(args.output_dir)
+    if args.reset_output_dir:
         for entity in opath.iterdir():
             if entity.is_symlink() or entity.is_file():
                 os.remove(str(entity.resolve()))
@@ -47,21 +131,28 @@ def main(input_directory: str,output_dir: str,reset_output_dir: bool=False,color
     for _ in range(0,os.cpu_count()**2):
         ppool.append([None])
     for input_entity in ipath.iterdir():
-        name=str(input_entity.relative_to(input_directory))        
+        name=str(input_entity.relative_to(args.input_directory))        
         output=opath.joinpath(name)
-        if not multiprocessing:        
-            image_process.main(img_name=str(input_entity),img_out_name=str(output),colorhex=colorhex,invert=invert,cell_invert=cell_invert)    
+        if not args.multiprocessing:        
+            image_process.main(img_name=str(input_entity),img_out_name=str(output),colorhex=args.colorhex,invert=args.invert,cell_invert=args.cell_invert)    
         else:
-            subprocess_it(img_name=str(input_entity),img_out_name=str(output),colorhex=colorhex,invert=invert,cell_invert=cell_invert,ppool=ppool)
-    if multiprocessing:
+            subprocess_it(img_name=str(input_entity),img_out_name=str(output),colorhex=args.colorhex,invert=args.invert,cell_invert=args.cell_invert,ppool=ppool)
+    if args.multiprocessing:
         drain(ppool=ppool)
 
 if __name__=='__main__':
-    if len(sys.argv) == 3:
-        main(input_directory=sys.argv[1],output_dir=sys.argv[2])
-    elif len(sys.argv) == 4:
-        main(input_directory=sys.argv[1],output_dir=sys.argv[2],colorhex=sys.argv[3])
-    elif len(sys.argv) == 5:
-        main(input_directory=sys.argv[1],output_dir=sys.argv[2],colorhex=sys.argv[3],invert=sys.argv[4].lower() in ['y','t','yes','true','1'],cell_invert=sys.argv[5].lower() in ['y','t','yes','true','1'])
-    elif len(sys.argv) == 6:
-        main(input_directory=sys.argv[1],output_dir=sys.argv[2],colorhex=sys.argv[3],invert=sys.argv[4].lower() in ['y','t','yes','true','1'],cell_invert=sys.argv[5].lower() in ['y','t','yes','true','1'],multiprocessing=sys.argv[6].lower() in ['y','t','yes','true','1'])
+    cp=command_line_parser.CommandLineParser()
+    if cp.validate(sys.argv):                
+        cp.parse_args(sys.argv)    
+        bclh = MultiImageCommandLineOptionsHydrator()
+        out:list[MultiImageCommandLineOptions]=[]
+        ex_out:list[Exception]=[]
+        if bclh.hydrate_and_validate(cp,out,ex_out):
+            outx=out[0]            
+            if outx.validate() and not outx.help:                
+                main(args=outx)
+        else:
+            print('Not valid args')
+            traceback.print_exception(ex_out[0])
+    else:
+        print('Not valid args')
