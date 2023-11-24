c89b596c5a74ee8d21082f5dc95be98fcd02ae7e
Charles Montgomery <charles.montgomery@charter.net>
2023-11-23 19:09:31 -0600

    Creating pip installable packages

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='binary_mosaic_images',
+    version='0.1.0',    
+    description='Binary Mosaic Images',
+    url='https://github.com/chazzofalf/binary_mosaic_images',
+    author='Charles Montgomery',
+    author_email='charles.montgomery@charter.net',
+    license='',
+    packages=['binary_mosaic_images'],
+    install_requires=['Pillow==10.1.0'],
+
+    classifiers=[
+        'Development Status :: Development/Open Testing',
+        'Intended Audience :: Graphics',        
+        'Operating System :: POSIX :: Linux',  
+        'Programming Language :: Python :: 3.11',
+    ],
+)
